b6d5b53dbc403a1232fbee00d407285a0e625989
sangeeta-chaukulkar <91315460+sangeeta-chaukulkar@users.noreply.github.com>
2021-09-24 17:06:55 +0530

    secret key removed

--- ecommerce2/settings.py
+++ ecommerce2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xi6@8_zhm$6laa#3ibx4pzfy=i++6nuqh(3rml+vg=gkkk*kzx'
+#SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
